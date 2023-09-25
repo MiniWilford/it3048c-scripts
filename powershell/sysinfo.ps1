4a79f65aa7a109ec37f508369d7090d3d9b39734
MiniWilford <dejarnke@mail.uc.edu>
2023-09-25 16:39:16 -0400

    Completed Lab3 sending email and sysinfo

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -6,3 +6,14 @@ $IP = getIP
 
 Write-Host("This machine's IP is $IP")
 Write-Host("This machine's IP is {0}" -f $IP)
+
+$User = $env:Username
+$ver = $HOST.Version
+$UserHost = $env:COMPUTERNAME
+$DATE = Get-Date
+
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $UserHost. PowerShell $ver. Today's Date is $DATE."
+
+Write-Host($BODY)
+
+Send-MailMessage -To "dejarnke@mail.uc.edu" -From "ktbtut@gqmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
