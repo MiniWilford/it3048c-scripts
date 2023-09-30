05bef25f02f4c633016bcea3cce52d8adaad4f0e
MiniWilford <dejarnke@mail.uc.edu>
2023-09-30 01:05:41 -0400

    Added sockettest, myGetIP, and getLogData python files

--- python/nameage.py
+++ python/nameage.py
@@ -11,6 +11,15 @@ print("What is your name?")
 
 myName = input()
 
+# Control Logic
+while myName != "AJ":
+	if myName == "your name":
+		print("Ha Ha, very funny. Seriously, who are you?")
+		myName = input()
+	else:
+		print("That is not your name. Please, tell me your real name.")
+		myName = input()
+
 print("Hello, " + myName + ". That is a good name. How old are you?")
 
 myAge = int(input())
@@ -23,4 +32,17 @@ print("I wish I was %i years old" %(myAge*2))
 time.sleep(3)
 print("I'm tired. I go sleep sleep now.")
 
-#
+# If Logic
+if myAge < 13:
+	print("Learning young, that's good")
+elif myAge == 13:
+	print("You're a teenager now... that's cool, I guess")
+elif myAge > 13 and myAge < 30:
+	print("Still young, still learning...")
+elif myAge >= 30 and myAge < 65:
+	print("Now you're adulting.")
+else:
+	print("...you've lived a long time?")
+
+
+
