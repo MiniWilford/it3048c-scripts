7734e2b47405a4e36b59f989208e5b6920f4fefe
MiniWilford <dejarnke@mail.uc.edu>
2023-09-30 16:45:01 -0400

    Reworked logic implementing python3 and 2 versioning detection

--- labs/Lab5.py
+++ labs/Lab5.py
@@ -1,18 +1,19 @@
 import random
+import sys
+
+# Python Version
+python_version = int(sys.version[0])
+print(python_version)
 
 # Introduction
-print("Hello, guess a number between 0 and 10")		
+print("\n\nHello, guess a number between 0 and 10")		
 
 def shouldContinue(resp):
-	try:
-		if resp == "yes" or resp == "y":
-			print("\nContinuing...\n") 
-			return True 
-		else:
-			print("\nGoodbye!")
-			return False
-	except:
-		print("\nThe answer you given is invalid. Presuming termination...")
+	if resp == "yes" or resp == "y":
+		print("\nContinuing...\n") 
+		return True 
+	else:
+		print("\nGoodbye!")
 		return False
 
 # Guess The Number Program
@@ -22,20 +23,33 @@ rand_num = int(random.random() * 10)
 while running:
 	# Generate a random integer between 0 and 10
 	# Get user input and convert to integer
-	resp = int(input("\nEnter a number: "))
-	if(resp == rand_num):
-		print("\nYou guessed the number! Correct!")
-		# Ask user to continue
-		print("\nDo you wish to continue? Yes or no?")
-		answer = str(input()).lower() # Does not work on Python 2.7
-		running = shouldContinue(answer) # Return True or False
-		if running == True:
-			rand_num = int(random.random() * 10)
-	elif(resp < rand_num):
-		print("\nThe number is higher.")
-	elif(resp > rand_num):
-		print("\nThe number is lower.")
-	else:
-		print("\nThat is not a number, try again.\n")
-	
+	try:	
+		resp = int(input("\nEnter a number: "))
+	except:
+		print("\nYou did not enter an integer...")
+	try:
+		if(resp == rand_num):
+			print("\nYou guessed the number! Correct!")
+			# Ask user to continue (does not work if Python 2.X.X and below)
+			if(python_version > 2):
+				answer = str(input("\nDo you wish to continue? Yes or no: ")).lower()
+				running = shouldContinue(answer)
+			elif(python_version < 3):
+				print("Do you wish to continue? y / n: ")
+				answer_python_two = raw_input() #Raw_input is necessary in Python2, input is Python3
+				running = shouldContinue(answer_python_two)			
+			else:
+				print("Invalid Python Version")
+				break;
+			# Determine if to keep running
+			if running == True:
+				rand_num = int(random.random() * 10)
+		elif(resp < rand_num):
+			print("\nThe number is higher.")
+		elif(resp > rand_num):
+			print("\nThe number is lower.")
+		else:
+			print("You broke something")
+	except:
+		print("Invalid input, try again")
 	
