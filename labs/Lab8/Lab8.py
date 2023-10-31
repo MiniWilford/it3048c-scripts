a5caf6e2d3cb34fb9ce52ebd80b8271e0657a925
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2023-10-30 21:07:52 -0400

    Added Lab8 and logic for finding title and description

--- labs/Lab8/Lab8.py
+++ labs/Lab8/Lab8.py
@@ -1,4 +1,27 @@
 import requests as req
+import re as regex
 
 # Grab site 
-site = req.get()
+request = req.get('https://www.w3schools.com/html/html5_geolocation.asp')
+
+# Get Site content
+site = str(request.content)
+
+# Extract data
+page_title = regex.compile('<h1>(.*?)</h1>')
+page_description = regex.compile('<h2>(.*?)</h2>')
+
+# Print "content" for title
+# Credit: https://www.delftstack.com/howto/python/python-remove-html-tags/
+title = page_title.search(site).group()
+remove_tags = regex.compile("<.*?>")
+clean_title = regex.sub(remove_tags, "", title)
+
+# Print "content" for description
+description = page_description.search(site).group()
+remove_tags = regex.compile("<.*?>")
+clean_description = regex.sub(remove_tags, "", description)
+cleaner_description = str(clean_description.replace('\\', ''))
+
+print("The page is about " + clean_title)
+print("The description of the page is: " + cleaner_description)
